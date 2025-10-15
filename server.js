41262f41a46f7b3ab4ea63b515adbbb511f50765
DSoto-Code <soto.gutierrez.david.alejandro.com>
2025-10-14 20:51:38 -0600

    termino de actividad

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const { Cookie } = require("express-session")
 
-const express = requiere ('express')
-const session= reqiuere ('express-session')
+const express = require ('express')
+const session= require ('express-session')
 
 const app=express()
-const PORT=3000
+const PORT=1000
+
 
 app.use(express.json())
 
@@ -25,18 +26,22 @@ app.get('/inicio',(req, res)=>{
     
 });
 
-app.post('login',(req,res)=>{
+app.post('/login',(req,res)=>{
     const {usuario, password}=req.body
-    if(usuario=='admin'&& password ==='1234'){
+    if(usuario=="admin"&& password ==="1234"){
         res.send('Inicio de sesion correcto')
     }else{
         res.status(401).send('Credenciales incorrectas')
     }
 })
 
-app.get('cerrar',(req, res)=>{
+app.get('/cerrar',(req, res)=>{
     req.session.destroy(err=>{
         if(err)return res.status(500).send('Error al cerrar sesion')
             res.send('sesion cerrada correctamente')
     })
+})
+
+app.listen(1000,()=>{
+    console.log('Server is running on port 1000');
 })
